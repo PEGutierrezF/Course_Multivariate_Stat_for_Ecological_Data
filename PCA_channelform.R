2c4259d6e041c2c0d4598d267177d73312bf43a8
Pablo E. Gutierrez-Fonseca <pabloe.gutierrezfonseca@gmail.com>
2021-12-23 12:49:54 -0600

    data

--- PCA_channelform.R
+++ PCA_channelform.R
@@ -19,13 +19,13 @@ library(ggbiplot)
 
 channel <- read.csv("data/PCA_channel_form.csv", header=TRUE)
 
-channel_1 <- select(channel, -Forma)
+channel_1 <- dplyr::select(channel, -Forma)
 summary(channel_1)
 
 
 # Imputate ----------------------------------------------------------------
 
-df1 <- select(channel_1, Elevacion, Ancho, Velocidad, Rocas, 
+df1 <- dplyr::select(channel_1, Elevacion, Ancho, Velocidad, Rocas, 
               Canto, grava, arena, Limo)
 
 df1_a <- missForest(df1, maxiter = 4, ntree = 100,
@@ -35,16 +35,15 @@ df1_a <- missForest(df1, maxiter = 4, ntree = 100,
                            xtrue = NA, parallelize = "no")
 class(df1_a$ximp)
 
-df2_a <- select(channel_1, Elevacion, NAtemp, NASatO2)
+df2_a <- dplyr::select(channel_1, Elevacion, NAtemp, NASatO2)
 
 
 # New data frame ----------------------------------------------------------
 
-
-new_channel <- do.call("merge", c(lapply(list(df1_a$ximp, df2_c), data.frame, 
+new_channel <- do.call("merge", c(lapply(list(df1_a$ximp, df2_a), data.frame, 
                       row.names=NULL), by = 0, all = TRUE, sort = FALSE))[-1]
 #remove 1 elevation
-new_channel_1 <- select(new_channel, -Elevacion.y)
+new_channel_1 <- dplyr::select(new_channel, -Elevacion.y)
 
 
 channel.pca <- prcomp(new_channel_1, center = TRUE, scale =TRUE)
